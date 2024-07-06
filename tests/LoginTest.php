b710fc9f3af6d7feb167fb45377b538c6de06215
agz-dev <agustintaverna.dev@outlook.com>
2024-07-06 14:54:43 -0300

    Resuelve conflictos de fusión

---
+++ tests/LoginTest.php
@@ -0,0 +1,16 @@
+<?php 
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use MiProyecto\Controladores\MiControlador;
+
+class LoginTest extends TestCase
+{
+    public function testLogin()
+    {
+/*         $controlador = new MiControlador();
+        $resultado = $controlador->miMetodo();
+        
+        $this->assertEquals('resultado esperado', $resultado); */
+    }
+}
